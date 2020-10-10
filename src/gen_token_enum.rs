49964c9ea4c438a6693c859546738ae6776f736d
amlo <xplzjwz@gmail.com>
2020-10-10 18:02:09 +0800

    WIP: codegen

--- src/gen_token_enum.rs
+++ src/gen_token_enum.rs
@@ -1,12 +1,12 @@
 use grammar::{Map, TokenId};
 use heck::CamelCase;
-use codegen::{Scope, Enum};
+use codegen::{Scope, Enum, DisVariant};
 
 pub fn gen(
   tokens: &Map<TokenId, String>,
   scope: &mut Scope,
 ) -> Map<TokenId, String> {
-  let mut en = Enum::new("TokenKind");
+  let mut en = Enum::<DisVariant>::new("TokenKind");
   en.vis("pub")
     .repr("u32")
     .derive("Debug")
@@ -18,7 +18,7 @@ pub fn gen(
   let mut token_names = Map::new();
   for (id, name) in tokens {
     let name = name.replace(&['\'', '-'][..], "_").to_camel_case();
-    en.new_variant(&format!("r#{} = {}", name, id.id()));
+    en.new_variant(name).discriminant(id.id().to_string());
     token_names.insert(*id, name);
   }
 
