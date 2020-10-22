75187192a03edca846c43cebf1dcde292533c5dc
amlo <xplzjwz@gmail.com>
2020-10-22 13:00:12 +0800

    feat: improve error reporting

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 #![feature(or_patterns)]
 
 use proc_macro::TokenStream;
-use proc_macro_error::proc_macro_error;
+use proc_macro_error::{proc_macro_error, emit_error, abort_call_site};
 use syn::*;
 use quote::{ToTokens, quote};
 use std::fs;
 use std::path::Path;
+use std::fmt;
 use parser_spec::ParserKind;
-use grammar::{GrammarError, GrammarErrorKind};
-use std::fmt::{self, Write};
 use codegen::{Module, Scope, Formatter, Format};
 use itertools::Itertools;
 
@@ -16,6 +15,7 @@ mod parser_spec;
 mod gen_token_enum;
 mod gen_lexer;
 mod gen_parser;
+mod report;
 
 #[proc_macro_error]
 #[proc_macro]
@@ -41,7 +41,12 @@ pub fn parsergen(expr: TokenStream) -> TokenStream {
 
   let parser = match parser {
     Ok(parser) => parser,
-    Err(mut err) => report(source_path, grammar, err.pop().unwrap()),
+    Err(errors) => {
+      for error in errors {
+        emit_error!("{}", report::report(&source_path, &grammar, error));
+      }
+      abort_call_site!("building parser failed");
+    }
   };
 
   let vis = spec.vis.to_token_stream().to_string();
@@ -128,148 +133,6 @@ fn gen_2d_table(
     .join(", ");
   let value = format!("[{}]", value);
   scope.new_static(table_name, ty).value(value);
-}
-
-fn report(
-  path: impl AsRef<Path>,
-  input: impl AsRef<str>,
-  err: lr::Error
-) -> ! {
-  match err {
-    lr::Error::GrammarError(err) => report_grammar_error(path, input, err),
-    lr::Error::ReduceReduceConflict(err) => report_rr_conflict(err),
-    lr::Error::ShiftReduceConflict(err) => report_sr_conflict(err),
-    lr::Error::PrecConflict(err) => report_prec_conflict(err),
-    lr::Error::AssocConflict(err) => report_assoc_conflict(err),
-  }
-}
-
-fn report_grammar_error(
-  path: impl AsRef<Path>,
-  input: impl AsRef<str>,
-  err: GrammarError
-) -> ! {
-  let lines = input.as_ref()[..err.span.0].split('\n').collect::<Vec<_>>();
-  let line = lines.len() + 1;
-  let col = lines.last().unwrap().chars().count() + 1;
-  let error = match err.kind {
-    GrammarErrorKind::MissingDecl => "missing declaration",
-    GrammarErrorKind::NameConflict => "name conflict",
-    GrammarErrorKind::ParseError => "syntax error",
-    GrammarErrorKind::NameNotFound => "name not found",
-    GrammarErrorKind::TokenDeclConflict => "token declaration conflict",
-  };
-
-  let mut buf = String::new();
-  writeln!(&mut buf,
-    "{} at {}:{}:{}",
-    error,
-    path.as_ref().display(),
-    line,
-    col
-  ).unwrap();
-  writeln!(&mut buf,
-    "message: {}", err.message
-  ).unwrap();
-
-  panic!("{}", buf)
-}
-
-fn report_rr_conflict(
-  err: lr::ReduceReduceConflictError
-) -> ! {
-  let mut buf = String::new();
-
-  writeln!(&mut buf,
-    "reduce-reduce conflict at state:\n"
-  ).unwrap();
-
-  for item in &err.state_items {
-    writeln!(&mut buf,
-      "  {}", item,
-    ).unwrap();
-  }
-
-  writeln!(&mut buf,
-    "\nwhich can be reduced by:\n\n  {}\n\nor:\n\n  {}\n\nwhen the lookahead is {}",
-    err.reduce1,
-    err.reduce2,
-    err.lookahead,
-  ).unwrap();
-
-  panic!("{}", buf)
-}
-
-fn report_sr_conflict(
-  err: lr::ShiftReduceConflictError
-) -> ! {
-  let mut buf = String::new();
-
-  writeln!(&mut buf,
-    "shift-reduce conflict at state:\n"
-  ).unwrap();
-
-  for item in &err.state_items {
-    writeln!(&mut buf,
-      "  {}", item,
-    ).unwrap();
-  }
-
-  writeln!(&mut buf,
-    "\nwhich can shift {}\nor reduce by:\n\n  {}",
-    err.shift,
-    err.reduce,
-  ).unwrap();
-
-  panic!("{}", buf)
-}
-
-fn report_prec_conflict(
-  err: lr::PrecConflictError
-) -> ! {
-  let mut buf = String::new();
-
-  writeln!(&mut buf,
-    "precedence conflict at state:\n"
-  ).unwrap();
-
-  for item in &err.state_items {
-    writeln!(&mut buf,
-      "  {}", item,
-    ).unwrap();
-  }
-
-  writeln!(&mut buf,
-    "\nthe production:\n\n  {}\n\nand the production:\n\n  {}\n\nhave different precedence",
-    err.prod1,
-    err.prod2,
-  ).unwrap();
-
-  panic!("{}", buf)
-}
-
-fn report_assoc_conflict(
-  err: lr::AssocConflictError
-) -> ! {
-  let mut buf = String::new();
-
-  writeln!(&mut buf,
-    "associativity conflict at state:\n"
-  ).unwrap();
-
-  for item in &err.state_items {
-    writeln!(&mut buf,
-      "  {}", item,
-    ).unwrap();
-  }
-
-  writeln!(&mut buf,
-    "\nthe production:\n\n  {}\n\nand the production:\n\n  {}\n\nhave different associativity",
-    err.prod1,
-    err.prod2,
-  ).unwrap();
-
-  panic!("{}", buf)
 }
 
 trait ExpectWith {
---
+++ src/report.rs
@@ -0,0 +1,145 @@
+use grammar::{GrammarError, GrammarErrorKind};
+use std::fmt::Write;
+use std::path::Path;
+
+pub(crate) fn report(
+  path: impl AsRef<Path>,
+  input: impl AsRef<str>,
+  err: lr::Error
+) -> String {
+  match err {
+    lr::Error::GrammarError(err) => report_grammar_error(path, input, err),
+    lr::Error::ReduceReduceConflict(err) => report_rr_conflict(err),
+    lr::Error::ShiftReduceConflict(err) => report_sr_conflict(err),
+    lr::Error::PrecConflict(err) => report_prec_conflict(err),
+    lr::Error::AssocConflict(err) => report_assoc_conflict(err),
+  }
+}
+
+fn report_grammar_error(
+  path: impl AsRef<Path>,
+  input: impl AsRef<str>,
+  err: GrammarError
+) -> String {
+  let lines = input.as_ref()[..err.span.0].split('\n').collect::<Vec<_>>();
+  let line = lines.len() + 1;
+  let col = lines.last().unwrap().chars().count() + 1;
+  let error = match err.kind {
+    GrammarErrorKind::MissingDecl => "missing declaration",
+    GrammarErrorKind::NameConflict => "name conflict",
+    GrammarErrorKind::ParseError => "syntax error",
+    GrammarErrorKind::NameNotFound => "name not found",
+    GrammarErrorKind::TokenDeclConflict => "token declaration conflict",
+  };
+
+  let mut buf = String::new();
+  writeln!(&mut buf,
+    "{} at {}:{}:{}",
+    error,
+    path.as_ref().display(),
+    line,
+    col
+  ).unwrap();
+  writeln!(&mut buf,
+    "message: {}", err.message
+  ).unwrap();
+
+  buf
+}
+
+fn report_rr_conflict(
+  err: lr::ReduceReduceConflictError
+) -> String {
+  let mut buf = String::new();
+
+  writeln!(&mut buf,
+    "reduce-reduce conflict at state:\n"
+  ).unwrap();
+
+  for item in &err.state_items {
+    writeln!(&mut buf,
+      "  {}", item,
+    ).unwrap();
+  }
+
+  writeln!(&mut buf,
+    "\nwhich can be reduced by:\n\n  {}\n\nor:\n\n  {}\n\nwhen the lookahead is {}",
+    err.reduce1,
+    err.reduce2,
+    err.lookahead,
+  ).unwrap();
+
+  buf
+}
+
+fn report_sr_conflict(
+  err: lr::ShiftReduceConflictError
+) -> String {
+  let mut buf = String::new();
+
+  writeln!(&mut buf,
+    "shift-reduce conflict at state:\n"
+  ).unwrap();
+
+  for item in &err.state_items {
+    writeln!(&mut buf,
+      "  {}", item,
+    ).unwrap();
+  }
+
+  writeln!(&mut buf,
+    "\nwhich can shift {}\nor reduce by:\n\n  {}",
+    err.shift,
+    err.reduce,
+  ).unwrap();
+
+  buf
+}
+
+fn report_prec_conflict(
+  err: lr::PrecConflictError
+) -> String {
+  let mut buf = String::new();
+
+  writeln!(&mut buf,
+    "precedence conflict at state:\n"
+  ).unwrap();
+
+  for item in &err.state_items {
+    writeln!(&mut buf,
+      "  {}", item,
+    ).unwrap();
+  }
+
+  writeln!(&mut buf,
+    "\nthe production:\n\n  {}\n\nand the production:\n\n  {}\n\nhave different precedence",
+    err.prod1,
+    err.prod2,
+  ).unwrap();
+
+  buf
+}
+
+fn report_assoc_conflict(
+  err: lr::AssocConflictError
+) -> String {
+  let mut buf = String::new();
+
+  writeln!(&mut buf,
+    "associativity conflict at state:\n"
+  ).unwrap();
+
+  for item in &err.state_items {
+    writeln!(&mut buf,
+      "  {}", item,
+    ).unwrap();
+  }
+
+  writeln!(&mut buf,
+    "\nthe production:\n\n  {}\n\nand the production:\n\n  {}\n\nhave different associativity",
+    err.prod1,
+    err.prod2,
+  ).unwrap();
+
+  buf
+}
